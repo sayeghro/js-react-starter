5c31168042c0f1b3d096cce2404be23d8e7237b7
Robert Sayegh <robert.sayegh@dali.dartmouth.edu>
2016-07-18 05:11:34 -0400

    fully functional base

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import Immutable from 'immutable';
 
-import Welcome from './welcome';
+import Note from './Note';
+import CreateBar from './CreateBar';
 
 // example class based component (smart component)
 class App extends Component {
@@ -8,13 +10,44 @@ class App extends Component {
     super(props);
 
     // init component state here
-    this.state = {};
+    this.state = {
+      notes: Immutable.Map(),
+    };
+
+    this.addNote = this.addNote.bind(this);
+    this.deleteNote = this.deleteNote.bind(this);
+    this.updateNote = this.updateNote.bind(this);
+  }
+  addNote(title) {
+    const id = Math.random().toString(36).substr(2, 9);
+    const noteInstance = {
+      title,
+      text: '',
+      x: 0,
+      y: 0,
+      zIndex: 0,
+    };
+    this.setState({
+      notes: this.state.notes.set(id, noteInstance),
+    });
+  }
+  deleteNote(id) {
+    this.setState({
+      notes: this.state.notes.delete(id),
+    });
+  }
+  updateNote(id, newNote) {
+    this.setState({
+      notes: this.state.notes.update(id, (note) => { return Object.assign({}, note, newNote); }),
+    });
+    console.log(this.state.notes);
   }
 
   render() {
     return (
       <div>
-        <Welcome />
+        <CreateBar addNote={this.addNote} />
+        {this.state.notes.entrySeq().map(([id, note]) => <Note key={id} id={id} note={note} deleteNote={this.deleteNote} updateNote={this.updateNote} />)}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/app';
 
 import './style.scss';
 
+
 // entry point that just renders app
 // could be used for routing at some point
 ReactDOM.render(<App />, document.getElementById('main'));
